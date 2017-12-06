49c5eacd1630cef13a08d4243753ae4f461f3bdb
Jiao Lin <linjiao@ornl.gov>
2017-12-06 10:23:29 -0500

    Refs #4. implemented

---
+++ dgsres/singlextal/fit2ee.py
@@ -0,0 +1,83 @@
+# fit mcvine-simulated point spread function to an empirical form
+
+import os, numpy as np
+import histogram.hdf as hh, histogram as H
+import scipy.optimize as sopt
+
+
+from . import fit_2d_psf
+class Fit2EE(fit_2d_psf.Fit):
+
+    """Eenrgy profile is sum of two exponential X error_func
+    """
+
+    def createModel(self):
+        self.model = EEAffineModel(self.Ei, self.E)
+        return
+
+    def fit_E_profile(self, sigma_left, sigma_right, weight_left, ef_width):
+        # fit res(E)
+        res_E = self.mcvine_psf_E
+        res_y = res_E.I.copy()
+        res_x = res_E.E.copy()
+        dx = res_x[1] - res_x[0]; res_y /= res_y.sum()*dx # normalize. density function
+        if isinstance(sigma_left, tuple):
+            # inputs are bounds
+            bounds = [sigma_left, sigma_right, weight_left, ef_width]
+            x0 = [np.average(b) for b in bounds]
+        else:
+            # inputs are single numbers
+            x0 = [sigma_left, sigma_right, weight_left, ef_width]
+            bounds = np.array([
+                [sigma_left/3, sigma_right/3, 0, ef_width/10.],
+                [sigma_left*3, sigma_right*3, 1., ef_width*10.]
+            ]).T
+        cost = self.model.make_E_profile_cost(res_x, res_y)
+        fitres = sopt.minimize(cost, x0=x0, bounds=bounds, method='L-BFGS-B')
+        E_profile = self.model.set_E_profile(*fitres.x)
+        yfit = E_profile(res_x); yfit /= yfit.sum()*dx
+        # fit parameters, x values, y values, fit y values, E_profile fitted
+        return fitres.x, res_x, res_y, yfit, E_profile
+
+
+class EEAffineModel(fit_2d_psf.PSF_Affine_Model):
+
+    """Energy profile is sum of two exponential X error_func
+    q profile is gaussian
+    Center of I(q) gaussians at different q is linearly associated with E
+    """
+
+    def update_parameters(self, sigma_left=None, sigma_right=None, weight_left=None, ef_width=None, q_sigma=None, dq_over_dE=None):
+        self.set_E_profile(sigma_left, sigma_right, weight_left, ef_width)
+        self.set_q_profile(q_sigma)
+        self.set_qE_profile(dq_over_dE)
+        return
+
+    def set_E_profile(self, sigma_left, sigma_right, weight_left, ef_width):
+        self.E_profile = self.make_E_profile_function(sigma_left, sigma_right, weight_left, ef_width)
+        return self.E_profile
+
+    def make_E_profile_function(self, sigma_left, sigma_right, weight_left, ef_width):
+        return EEModel(sigma_left, sigma_right, weight_left, ef_width)
+
+
+class EEModel:
+
+    def __init__(self, sigma_left, sigma_right, weight_left, ef_width):
+        self.sigma_left = sigma_left
+        self.sigma_right = sigma_right
+        self.weight_left = weight_left
+        self.ef_width = ef_width
+        return
+    
+    def __call__(self, x):
+        from scipy.special import erfc
+        weight_left = self.weight_left
+        weight_right = 1.0 - weight_left
+        sigma_left = self.sigma_left
+        sigma_right = self.sigma_right
+        ef_width = self.ef_width
+        t1 = fit_2d_psf.gaus(x, 1./np.sqrt(2*np.pi)/np.abs(sigma_left), 0., sigma_left) * (1-erfc(x/ef_width)/2.)
+        t2 = fit_2d_psf.gaus(x, 1./np.sqrt(2*np.pi)/np.abs(sigma_right), 0., sigma_right) * erfc(x/ef_width)/2.
+        return t1*weight_left + t2*weight_right
+
---
+++ tests/singlextal/test_fit2ee.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# Jiao Lin <jiao.lin@gmail.com>
+
+import os, numpy as np
+import mcvine.cli
+from dgsres.singlextal import fit2ee
+here = os.path.abspath(os.path.dirname(__file__))
+
+def test():
+    Ei = 12.
+    E0 = 5.
+    datadir = os.path.join(here, '..', 'data', 'PbTe-CNCS-HH4-Ei_12-E_5-q_0')
+    fitter = fit2ee.Fit2EE(datadir, qaxis=(-0.3, 0.3, 0.01), Eaxis=(-2, 1, 0.05), Ei=Ei, E=E0)
+    fitter.load_mcvine_psf_qE()
+    fitted_params, res_x, res_y, yfit, E_profile = fitter.fit_E_profile(sigma_left=0.2, sigma_right=0.2, ef_width=0.1, weight_left=0.5)
+    print fitted_params
+    popt, res_x, res_y, yfit, q_profile = fitter.fit_q_profile(sigma=0.1)
+    fitted_parameters, qgrid, Egrid, res_z, zfit, qE_profile = fitter.fit_qE_profile(dq_over_dE=0.02)
+    print fitted_parameters
+    return
+
+def main():
+    test()
+    return
+
+
+if __name__ == '__main__': main()
+
+# End of file 
