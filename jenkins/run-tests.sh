af76809aedcec4340483052cead62731ad7a3676
Jiao Lin <linjiao@ornl.gov>
2018-04-16 16:58:53 -0400

    install pytest before running tests

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -3,12 +3,13 @@
 echo "Testing dgsres"
 
 export PATH=$HOME/mc/bin:$PATH
-conda remove test-dgsres
+conda remove test-dgsres --all
 conda create -n test-dgsres
 source activate test-dgsres
 
 set -e
 conda config --add channels mantid  # need mantid-framework
+conda install pytest
 conda install -c mcvine/label/unstable mcvine
 conda install mantid-framework
 conda list mcvine
