3336313ea18afc8403577445b34ac2d857c14e4f
Jiao Lin <linjiao@ornl.gov>
2018-04-17 10:46:10 -0400

    Refs #3. added "install.sh"

---
+++ jenkins/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Install"
+
+export PATH=$HOME/mc/bin:$PATH
+conda remove -n test-dgsres --all
+conda create -n test-dgsres
+source activate test-dgsres
+
+conda config --add channels mantid  # need mantid-framework
+conda install -c mcvine/label/unstable mcvine
--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -3,20 +3,15 @@
 echo "Testing dgsres"
 
 export PATH=$HOME/mc/bin:$PATH
-conda remove -n test-dgsres --all
-conda create -n test-dgsres
 source activate test-dgsres
 
 set -e
-conda config --add channels mantid  # need mantid-framework
-conda install -c mcvine/label/unstable mcvine
+
+# check mcvine
+conda list mcvine
 
 # check mantid
 python -c "import mantid"
 
 conda install pytest
-# conda install mantid-framework
-conda list mcvine
-
-export AWS_S3_PROFILE_NAME=ndav_mcvine
 py.test -s
