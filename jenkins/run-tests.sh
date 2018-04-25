ada6f82cc68b90efcb9876151d04882186159837
Jiao Lin <linjiao@ornl.gov>
2018-04-25 11:47:46 -0400

    Refs #3. jenkins/run-tests.sh: only bail out when failed running "py.test -s"

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -5,13 +5,12 @@ echo "Testing dgsres"
 export PATH=$HOME/mc/bin:$PATH
 source activate test-dgsres
 
-set -e
-
 # check mcvine
 conda list mcvine
 
 # check mantid
 python -c "import mantid"
 
+set -e
 conda install pytest
 py.test -s
