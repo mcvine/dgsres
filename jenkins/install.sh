9ed33110e6fb7486afc48412525eaacb47db96cf
Jiao Lin <linjiao@ornl.gov>
2018-05-10 00:09:50 -0400

    Refs mcvine/devel#9. dgsres/jenkins: add channel "diffpy"

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -7,5 +7,7 @@ conda remove -n test-dgsres --all
 conda create -n test-dgsres
 source activate test-dgsres
 
+conda config --add channels conda-forge
+conda config --add channels diffpy
 conda config --add channels mantid  # need mantid-framework
 conda install -c mcvine/label/unstable mcvine
