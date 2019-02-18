72808438bfb8851ccd02ede55445a23adf0df305
Jiao Lin <linjiao@ornl.gov>
2019-02-17 19:58:01 -0500

    fit_2d_psf.py: added parameter "adjust_energy_center"; take a cut to get I(Q) for Q-profile instead of summing everything together

--- dgsres/singlextal/fit_2d_psf.py
+++ dgsres/singlextal/fit_2d_psf.py
@@ -21,26 +21,33 @@ class Fit(object):
         self.model = None
         return
 
-    def load_mcvine_psf_qE(self):
+    def load_mcvine_psf_qE(self, adjust_energy_center=False):
         path = self.sim_path
         qaxis = self.qaxis; Eaxis = self.Eaxis
         dEs = np.load(os.path.join(path, 'dEs.npy'))
         dqs = np.load(os.path.join(path, 'dxs.npy'))
         probs = np.load(os.path.join(path, 'probs.npy'))
         hist, qedges, Eedges = np.histogram2d(dqs, dEs, bins=(np.arange(*qaxis), np.arange(*Eaxis)), weights=probs)
-        qaxis = H.axis('q', boundaries=qedges)
-        Eaxis = H.axis('E', boundaries=Eedges)
         # remove outliers
         median = np.median(hist[hist>0])
         normal = hist[hist<median*100]; normal_max = np.max(normal)
         hist[hist>median*100] = normal_max
+        # adjust energy center
+        if adjust_energy_center:
+            hist_E = hist.sum(axis=0)
+            Ebincenters = (Eedges[1:] + Eedges[:-1])/2
+            Ecenter = (Ebincenters*hist_E).sum()/hist_E.sum()
+            Eedges -= Ecenter
         # res(q, E) histogram
+        qaxis = H.axis('q', boundaries=qedges)
+        Eaxis = H.axis('E', boundaries=Eedges)
         self.mcvine_psf_qE = reshist  = H.histogram('res', (qaxis, Eaxis), data=hist)
         self.qEgrids = np.meshgrid(reshist.q, reshist.E)
         # res(E) histogram
         self.mcvine_psf_E = reshist.sum('q')
         # res(q) histogram
-        self.mcvine_psf_q = reshist.sum('E')
+        Emin = Eedges[0]; Emax = Eedges[-1]; middle = (Emin+Emax)/2; Erange = Emax-Emin
+        self.mcvine_psf_q = reshist[(), (middle-Erange/20, middle+Erange/20)].sum('E')
         return
 
     def fit_E_profile(self, a=None, b=None, R=None, sigma=None, t0=None):
@@ -108,6 +115,7 @@ class PSF_Affine_Model(object):
         self.E = E
         self.E_profile = None
         self.q_profile = None
+        self.dq_over_dE = 0
         return
 
     def set_qE_profile(self, dq_over_dE):
@@ -138,10 +146,7 @@ class PSF_Affine_Model(object):
         raise NotImplementedError("set_E_profile")
 
     def set_q_profile(self, sigma):
-        def _(x):
-            h = 1./np.sqrt(2*np.pi)/np.abs(sigma)
-            return gaus(x, h, 0., sigma)
-        self.q_profile = _
+        self.q_profile = Qprofile(sigma)
         return self.q_profile
     
     def make_E_profile_function(self, a, b, R, sigma=None, t0=None):
@@ -155,6 +160,16 @@ class PSF_Affine_Model(object):
             yfit /= np.sum(yfit) * dx
             return np.sum((yfit-res_y)**2)/res_y.size
         return cost
+
+
+class Qprofile:
+
+    def __init__(self, sigma):
+        self.sigma = sigma
+        self._h = 1./np.sqrt(2*np.pi)/np.abs(sigma)
+
+    def __call__(self, x):
+        return gaus(x, self._h, 0., self.sigma)
 
 
 def gaus(x,a,x0,sigma):
