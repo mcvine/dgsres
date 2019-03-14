270d822ca117c1e8afa8d9ed18e99ec9b85c59f9
Jiao Lin <linjiao@ornl.gov>
2019-03-13 22:26:14 -0400

    sim_workflow: added methods to simulate and plot points on a grid

--- dgsres/singlextal/sim_workflow.py
+++ dgsres/singlextal/sim_workflow.py
@@ -1,3 +1,7 @@
+"""
+see https://jupyter.sns.gov/user/{USER}/notebooks/data/SNS/SEQ/IPTS-16800/shared/resolution/resolution%20simulations%20-%20improve%20workflow.ipynb#
+"""
+
 import os, shutil, subprocess as sp, time
 import numpy as np
 from . import use_res_comps
@@ -38,3 +42,52 @@ def simulate(q, E, slice, outdir, config, Nrounds_beam=1):
     duration = end - start
     print "* simulation took %s seconds" % duration
     return out
+
+
+def simulate_all_grid_points(slice, config, Nrounds_beam=1):
+    failed = []; outputs = {}
+    outdir_pattern = config.simdir_pattern
+    for q in slice.grid.qaxis.ticks():
+        for E in slice.grid.Eaxis.ticks():
+            simdir = outdir_pattern % (q,E)
+            try:
+                outputs[(q,E)] = simulate(q=q, E=E, slice=slice, outdir=simdir, config=config, Nrounds_beam=Nrounds_beam)
+            except:
+                failed.append( (q,E) )
+        continue
+    return outputs, failed
+
+
+def plot_resolution_on_grid(slice, config, figsize=(10, 7)):
+    from matplotlib import pyplot as plt
+    qs = slice.grid.qaxis.ticks()
+    Es = slice.grid.Eaxis.ticks()
+    ncols = len(qs)
+    nrows = len(Es)
+    res_2d_grid = slice.res_2d_grid
+    hkl_projection = slice.hkl_projection
+    fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
+    for irow in range(nrows):
+        for icol in range(ncols):
+            q = qs[icol]
+            E = Es[irow]
+            simdir = config.simdir_pattern % (q,E)
+            try:
+                probs = np.load('%s/probs.npy' % simdir)
+            except IOError:
+                continue
+            dEs = np.load('%s/dEs.npy' % simdir)
+            dhkls = np.load('%s/dhkls.npy' % simdir)
+            dqs = np.dot(dhkls, hkl_projection)/np.linalg.norm(hkl_projection)**2
+            I, dqedges, dEedges = np.histogram2d(
+                bins=(res_2d_grid.qaxis.ticks(), res_2d_grid.Eaxis.ticks()), weights=probs, x=dqs, y=dEs )
+            dqg, dEg = np.meshgrid(dqedges, dEedges)
+            ax1 = axes[irow][icol]
+            median = np.nanmedian(I[I>0])
+            good = I[I<median*200]
+            goodmax = good.max()
+            I[I>median*100] = goodmax
+            ax1.set_title("q=%.2f, E=%.2f" % (q, E))
+            ax1.pcolormesh(dqg, dEg, I.T)
+    plt.tight_layout()
+    return
