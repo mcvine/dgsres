cc3f04dcbdd0c6ae8f7a8133bf74cc6037d6c376
Jiao Lin <jiao.lin@gmail.com>
2021-02-12 20:16:09 -0800

    openmpi requires hostfile:   builders/aws-codebuild/test.sh

--- builders/aws-codebuild/test.sh
+++ builders/aws-codebuild/test.sh
@@ -17,4 +17,9 @@ python -c "import mcvine"
 mcvine
 
 python setup.py install
-py.test 
+
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+echo "localhost slots=8" > $(dirname $(dirname $(which python)))/etc/openmpi-default-hostfile
+
+py.test
