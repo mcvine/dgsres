b70d6184aa110928c062c8be5421b32b8e2eb8f1
Jiao Lin <jiao.lin@gmail.com>
2021-07-08 08:10:33 -0700

    restore tests:   builders/aws-codebuild/test.sh

--- builders/aws-codebuild/test.sh
+++ builders/aws-codebuild/test.sh
@@ -22,5 +22,4 @@ export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 echo "localhost slots=8" > $(dirname $(dirname $(which python)))/etc/openmpi-default-hostfile
 
-# py.test
-python tests/singlextal/test_AMATERAS_workflow.py
+py.test
