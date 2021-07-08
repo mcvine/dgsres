fa5d003c9c1ea5b0dc82e3430e1195b93f4b2a40
Jiao Lin <jiao.lin@gmail.com>
2021-07-08 06:03:23 -0700

    install prerequisites such as latex and lmfit

--- builders/aws-codebuild/install-deps.sh
+++ builders/aws-codebuild/install-deps.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 export PATH=$HOME/mc/bin:$PATH
-conda create -n test tbb=2020.3 mcvine=1.4 tqdm pytest awscli cloudpickle python=$PYTHON_VERSION
+conda create -n test tbb=2020.3 mcvine=1.4 tqdm pytest awscli cloudpickle lmfit pylatex scikit-image=0.15 python=$PYTHON_VERSION
 source activate test
 conda list mcvine
 conda list mcvine-core
