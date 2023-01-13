d35467cf3fb1260ca5aece3a96b1c9d43287b3db
Garrett E. Granroth <granrothge@ornl.gov>
2023-01-13 15:23:36 -0500

    add No cache to test

--- tests/singlextal/test_workflow_fit.py
+++ tests/singlextal/test_workflow_fit.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 #
 # Garrett Granroth <granrothge@ornl.gov>
-import os, numpy as np, lmfit
+import os, numpy as np
+import unittest
 from dgsres.singlextal.PSF_Affine_Model import gaus
-from dgsres.singlextal.fit2ee import EEModel
-from dgsres.singlextal.fit_2d_psf import Fit 
+
 from dgsres.singlextal.workflow import fit
 import imp
 import time
 here = os.path.abspath(os.path.dirname(__file__))
 
 st = time.time()
-def test():
-    config = imp.load_source('config', os.path.join(here,'SEQUOIA_data','Res_config_file_29.py'))
-    workdir = os.path.join(here,'SEQUOIA_data','')
-    os.chdir(workdir)
-    print("load source {}".format(time.time()-st))
-    sl=config.slices[0]
-    q = 0.5 
-    E = 12.0
-    #datadir = os.path.join(config.thisdir,config.simdir(q,E,config.slices[0]))
-    fitter = fit(q,E,sl,config,use_cache=True)
-def main():
-    test()
-    return
+class TestCase(unittest.TestCase):
+    def test_NO_cache(self):
+        config = imp.load_source('config', os.path.join(here,'SEQUOIA_data','Res_config_file_29.py'))
+        workdir = os.path.join(here,'SEQUOIA_data','')
+        os.chdir(workdir)
+        print("load source {}".format(time.time()-st))
+        sl=config.slices[0]
+        q = 0.5 
+        E = 12.0
+        #datadir = os.path.join(config.thisdir,config.simdir(q,E,config.slices[0]))
+        fitter = fit(q,E,sl,config,use_cache=True)
+        return
+    def test_cache(self):
+        config = imp.load_source('config', os.path.join(here,'SEQUOIA_data','Res_config_file_29.py'))
+        workdir = os.path.join(here,'SEQUOIA_data','')
+        os.chdir(workdir)
+        print("load source {}".format(time.time()-st))
+        sl=config.slices[0]
+        q = 0.5 
+        E = 12.0
+        #datadir = os.path.join(config.thisdir,config.simdir(q,E,config.slices[0]))
+        fitter = fit(q,E,sl,config,use_cache=True)
+        return
 
-
-if __name__ == '__main__': main()
+if __name__ == '__main__': 
+    unittest.main()
 
 # End of file
