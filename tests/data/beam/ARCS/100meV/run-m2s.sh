945a43785f02ee571575c479e8404a5250fc0ef0
Jiao Lin <jiao.lin@gmail.com>
2023-01-16 12:36:06 -0800

    added tests/data/beam/ARCS/100meV

---
+++ tests/data/beam/ARCS/100meV/run-m2s.sh
@@ -0,0 +1,1 @@
+mcvine instruments arcs mod2sample --ncount=1000000000.0 --buffer_size=100000000 --output-dir=_m2sout --moderator.S_filename=/home/linjiao/miniconda3/envs/mcvine/share/mcvine/resources/instruments/ARCS/moderator/source_sct521_bu_17_1.dat --mpirun.nodes=10
