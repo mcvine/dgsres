1175977f20aff27e2d5f5392904011c0ec5ba256
Garrett E. Granroth <granrothge@ornl.gov>
2023-01-19 13:44:44 -0500

    updated to actually run via pytest

--- tests/singlextal/test_workflow_fit.py
+++ tests/singlextal/test_workflow_fit.py
@@ -2,7 +2,7 @@
 #
 # Garrett Granroth <granrothge@ornl.gov>
 import os, numpy as np
-import unittest
+import pytest
 from dgsres.singlextal.PSF_Affine_Model import gaus
 
 from dgsres.singlextal.workflow import fit
@@ -10,10 +10,12 @@ import imp
 import time
 import cloudpickle as pkl
 interactive=False
-here = os.path.abspath(os.path.dirname(__file__))
+
 ts = time.time()
 
-def init_dec(func):
+@pytest.fixture
+def init_dec():
+    here = os.path.abspath(os.path.dirname(__file__))
     config = imp.load_source('config', os.path.join(here,'SEQUOIA_data','Res_config_file_29.py'))
     workdir = os.path.join(here,'SEQUOIA_data','')
     with open(os.path.join(here,'SEQUOIA_data','cnf-1p5k0-q_0.500-E_12.000-fitter.pkl'), 'rb') as fh:
@@ -22,23 +24,26 @@ def init_dec(func):
     sl = config.slices[0]
     q = 0.5
     E = 12.0
-    func(q,E,sl,config,fit_chk)
     print("time = {} s".format(time.time()-ts))
+    return [q,E,sl,config,fit_chk]
 
 
-@init_dec
-def test_NO_cache(q,E,sl,config,fit_chk):
-    #datadir = os.path.join(config.thisdir,config.simdir(q,E,config.slices[0]))
-    fitter = fit(q,E,sl,config,use_cache=False)
+
+def test_NO_cache(init_dec):
+    fp = init_dec[:-1]
+    fit_chk = init_dec[-1]
+    fitter = fit(*fp,use_cache=False)
     tnum = np.sum((fitter.fit_result.best_fit-fit_chk.fit_result.best_fit)**2/fitter.fit_result.best_fit**2)
     tdnm = np.sum(1/fitter.fit_result.best_fit**2)
     print (np.sqrt(tnum/tdnm))
     assert np.sqrt(tnum/tdnm) < 1e-8
     return
 
-@init_dec
-def test_cache(q,E,sl,config,fit_chk):
-    fitter = fit(q,E,sl,config,use_cache=True)
+
+def test_cache(init_dec):
+    fp = init_dec[:-1]
+    fit_chk = init_dec[-1]
+    fitter = fit(*fp,use_cache=True)
     tnum = np.sum((fitter.fit_result.best_fit-fit_chk.fit_result.best_fit)**2/fitter.fit_result.best_fit**2)
     tdnm = np.sum(1/fitter.fit_result.best_fit**2)
     print (np.sqrt(tnum/tdnm))
@@ -46,20 +51,17 @@ def test_cache(q,E,sl,config,fit_chk):
     os.remove('1p5k0-q_0.500-E_12.000-fitter.pkl')
     return
 
-@init_dec
-def test_NO_multiprocessor(q,E,sl,config,fit_chk):
-    fitter = fit(q,E,sl,config,use_cache=False,multiprocess=False)
+
+def test_NO_multiprocessor(init_dec):
+    fp = init_dec[:-1]
+    fit_chk = init_dec[-1]
+    fitter = fit(*fp,use_cache=False,multiprocess=False)
     tnum = np.sum((fitter.fit_result.best_fit-fit_chk.fit_result.best_fit)**2/fitter.fit_result.best_fit**2)
     tdnm = np.sum(1/fitter.fit_result.best_fit**2)
     print (np.sqrt(tnum/tdnm))
     assert np.sqrt(tnum/tdnm) < 1e-8
     return
 
-def main():
-    test_NO_cache()
-    test_cache()
-    test_NO_multiprocessor()
 
-#if __name__ == '__main__': main()
 
 # End of file
