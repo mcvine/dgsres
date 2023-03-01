9c35fa1aadfd02db9171530fb9f5a935bbdd07e2
Garrett E. Granroth <granrothge@ornl.gov>
2023-03-01 17:29:52 -0500

    added tests for reading parameters from mdh

--- tests/singlextal/test_mdh_tools.py
+++ tests/singlextal/test_mdh_tools.py
@@ -2,8 +2,9 @@
 #
 # Garrett Granroth <granrothge@ornl.gov>
 import unittest
-from dgsres.singlextal.mdh_tools import gen_lattice_vectors
+from dgsres.singlextal.mdh_tools import gen_lattice_vectors,slice_from_MDH
 import numpy as np
+import os
 
 class tests(unittest.TestCase):
     def test_latt_vec_ortho(self):
@@ -28,6 +29,19 @@ class tests(unittest.TestCase):
         vs = gen_lattice_vectors(8.478, 9.572, 10.487, 73.46, 68.591, 64.762)
         self.assertTrue(np.all(np.abs(vs[0]-np.array([8.478, 0, 0]))/8.478<1e-4))
         self.assertTrue(np.all(np.abs(vs[1]-np.array([4.081, 8.658, 0]))/9.572<1e-4))
-        self.assertTrue(np.all(np.abs(vs[2]-np.array([3.828, 1.496, 9.648]))/10.487<1e-4))    
+        self.assertTrue(np.all(np.abs(vs[2]-np.array([3.828, 1.496, 9.648]))/10.487<1e-4))
+
+    def test_slice_from_MDH(self):
+        MDH_path = os.path.join('SEQUOIA_data','slice_0p5K0E_28meV_4K.nxs')
+        sl = slice_from_MDH(MDH_path,'test')
+        self.assertTrue(sl.name=='test')
+        self.assertTrue(np.all(sl.hkl_projection-np.array([0,1,0])<1e-8))
+        self.assertTrue(np.all(sl.hkl0-np.array([0.5,0,0])<1e-8))
+        self.assertTrue(abs(sl.grid.qaxis.min+3.975)<1e-6)
+        self.assertTrue(abs(sl.grid.qaxis.max-3.975)<1e-6)
+        self.assertTrue(abs(sl.grid.qaxis.step-0.05)<1e-6)
+        self.assertTrue(abs(sl.grid.Eaxis.min+2.925)<1e-6)
+        self.assertTrue(abs(sl.grid.Eaxis.max-26.925)<5e-6)
+        self.assertTrue(abs(sl.grid.Eaxis.step-.15)<1e-6)
 
 if __name__ == '__main__': unittest.main()
